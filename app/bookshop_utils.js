3f3eb8dedb2283a2056eb4f430a952fc4549ed1e
Holly <hollyduckworth@hotmail.co.uk>
2020-03-24 20:09:01 +0000

    Holly answers to programming_fundamentals !

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,38 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(first, second) {
+    return (first + " " + second);
+}
+
+function makeHalfPrice(price) {
+    return price * 0.5;
+}
+
+function countBooks(books) {
+    return (books.length);
+}
+
+function inStock(book) {
+    if ((book.quantity)>0) {
+    return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function getTotalOrderPrice(price, quantity) {
+    let calc = (price * quantity) * 1.2
+    let fixed = Math.round(calc * 1e2) / 1e2
+    return fixed;
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    inStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,57 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
 });
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 50 when passed 100", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns 3 when put in an array of 3 books", () => {
+    expect(utils.countBooks(["peppa pig", "the wasp factory", "adrian mole"])).toBe(3);
+  });
+});
+
+describe("utils.inStock", () => {
+  test("returns true if the title is in stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 9
+    };
+  
+    expect(utils.inStock(book)).toBe(true);
+  });
+
+  test("returns false if the title is not in stock", () => {
+    const book = {
+      title: "Adrian Mole",
+      author: "Sue Townsend",
+      yearOfPublication: 1981,
+      quantity: 0
+    };
+  
+    expect(utils.inStock(book)).toBe(false);
+  });
+});
+
+describe("utils.getTotalOrderPrice", () => {
+  test("returns 43.2 when passed 18 pound price and 2 books", () => {
+    expect(utils.getTotalOrderPrice(18, 2)).toBe(43.2);
+  });
+});
+
